5cd0ad24743e5109f20882ab8773bf78ebf6bd94
Jim Colinsworth <collinsworth-jim@norc.org>
2023-11-08 13:14:59 -0600

    logging update

--- crimedata/crimedata/pipelines.py
+++ crimedata/crimedata/pipelines.py
@@ -7,16 +7,17 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import json
+import logging as log
 
 class JsonlWriterPipeline:
     def open_spider(self, spider):
-        print(f"-------{dir(self)}")
+        log.info(f"-------{dir(self)}")
         self.file = open("items.jsonl", "w")
 
     def close_spider(self, spider):
         self.file.close()
 
     def process_item(self, item, spider):
-        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+        line = json.dumps(ItemAdapter(item).asdict(), indent=4) + "\n"
         self.file.write(line)
         return item
--- crimedata/crimedata/spiders/socrata.py
+++ crimedata/crimedata/spiders/socrata.py
@@ -1,11 +1,12 @@
 import scrapy
 from pathlib import Path
+import logging as log
 
 class SocrataSpider(scrapy.Spider):
     name = "socrata"
     urls = {
-        "https://data.nashville.gov/api/views/2u6v-ujjs/rows.csv?date=20231107&accessType=DOWNLOAD": "nashville-tn-city-incidents",
-        "https://data.memphistn.gov/api/views/ybsi-jur4/rows.csv?date=20231107&accessType=DOWNLOAD": "memphis-tn-city-incidents",
+        #"https://data.nashville.gov/api/views/2u6v-ujjs/rows.csv?date=20231107&accessType=DOWNLOAD": "nashville-tn-city-incidents",
+        #"https://data.memphistn.gov/api/views/ybsi-jur4/rows.csv?date=20231107&accessType=DOWNLOAD": "memphis-tn-city-incidents",
         "https://data.hartford.gov/api/views/w8n8-xfuk/rows.csv?date=20231107&accessType=DOWNLOAD": "hartford-ct-city-incidents",
         "https://data.kcmo.org/api/views/bfyq-5nh6/rows.csv?date=20231107&accessType=DOWNLOAD": "kansas-city-mo-city-incidents",
         "https://data.littlerock.gov/api/views/bz82-34ep/rows.csv?date=20231107&accessType=DOWNLOAD": "littlerock-ak-city-incidents",
@@ -20,6 +21,6 @@ class SocrataSpider(scrapy.Spider):
         name = self.urls[response.url]
         filename = f"socrata-{name}.csv"
         Path(filename).write_text(response.text)
-        self.log(f"Saved file {filename}")
+        log.info(f"Saved file {filename}")
         
 
